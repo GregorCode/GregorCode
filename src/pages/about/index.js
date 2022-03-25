b3eddb8cb621b5194d4ed0d1389767a5b8eb4ff0
Gregorys <gregorysgonzalez@gmail.com>
2022-03-25 00:55:05 -0300

    arreglo el estilo del code con white-space

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -7,16 +7,16 @@ const About = () => {
   const { t } = useTranslation('about');
   return (
     <Layout>
-      <div className="flex rounded-xl shadow-2xl border mt-10 bg-gray-100 dark:bg-gray-800">
-        <Image priority src="/images/profile.jpg" alt="Gregor" className="rounded-lg" height={250} width={800} />
-        <div className="md:p-6 text-center md:text-left space-y-3">
+      <div className="flex sm:flex-col md:flex-row rounded-xl shadow-2xl border mt-10 bg-gray-100 dark:bg-gray-800">
+        <Image priority src="/images/profile.jpg" alt="Gregor" className="rounded-lg" height="800" width="1000" />
+        <div className="text-center md:text-left sm:text-left sm:p-6 md:p-6 sm:space-y-2 md:space-y-3">
           <figcaption>
-            <div className="text-2xl font-bold text-gray-900 dark:text-white">{Autor}</div>
-            <div className="text-sm text-gray-500 dark:text-gray-400">{t('Ocupacion_Pais')}</div>
+            <div className="sm:text-base md:text-xl font-bold text-gray-900 dark:text-white">{Autor}</div>
+            <div className="sm:text-xs md:text-sm text-gray-500 dark:text-gray-400">{t('Ocupacion_Pais')}</div>
           </figcaption>
-          <p className="text-sm">{t('Presentacion')}</p>
-          <p className="text-sm">{t('Estudio_Actual')}</p>
-          <p className="font-bold">{t('Slogan')}</p>
+          <p className="sm:text-xs md:text-sm">{t('Presentacion')}</p>
+          <p className="sm:text-xs md:text-sm">{t('Estudio_Actual')}</p>
+          <p className="sm:text-sm md:text-base font-bold">{t('Slogan')}</p>
         </div>
       </div>
     </Layout>
